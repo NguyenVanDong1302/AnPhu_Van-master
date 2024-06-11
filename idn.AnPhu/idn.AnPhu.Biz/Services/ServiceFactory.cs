eb8b6f8096c9477f82b241da1715bc24c4ba70fd
NguyenVanDong1302 <dongyh2002bghs@gmail.com>
2024-06-11 17:08:17 +0700

    Git Commit

---
+++ idn.AnPhu/idn.AnPhu.Biz/Services/ServiceFactory.cs
@@ -0,0 +1,58 @@
+﻿using idn.AnPhu.Biz.Persistance.SqlServer;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace idn.AnPhu.Biz.Services
+{
+    public class ServiceFactory
+    {
+        static Hashtable services = new Hashtable();
+
+        static ServiceFactory()
+        {
+            #region["Auth"]
+            services.Add(typeof(Sys_UserManager), new Sys_UserManager(new Sys_UserProvider()));
+            services.Add(typeof(Sys_GroupManager), new Sys_GroupManager(new Sys_GroupProvider()));
+            #endregion
+        }
+
+        public static T GetService<T>()
+        {
+            foreach (var service in services.Values)
+            {
+                if (service is T)
+                {
+                    return (T)service;
+                }
+            }
+            return default(T);
+        }
+
+        public static Sys_UserManager Sys_UserManager
+        {
+            get
+            {
+                return (Sys_UserManager)services[typeof(Sys_UserManager)];
+            }
+            set
+            {
+                services[typeof(Sys_UserManager)] = value;
+            }
+        }
+        public static Sys_GroupManager Sys_GroupManager
+        {
+            get
+            {
+                return (Sys_GroupManager)services[typeof(Sys_GroupManager)];
+            }
+            set
+            {
+                services[typeof(Sys_GroupManager)] = value;
+            }
+        }
+    }
+}
---
+++ idn.AnPhu/idn.AnPhu.Website/Areas/Auth/AuthAreaRegistration.cs
@@ -0,0 +1,98 @@
+﻿using System.Web.Mvc;
+
+namespace idn.AnPhu.Website.Areas.Auth
+{
+    public class AuthAreaRegistration : AreaRegistration 
+    {
+        public override string AreaName 
+        {
+            get 
+            {
+                return "Auth";
+            }
+        }
+
+        public override void RegisterArea(AreaRegistrationContext context) 
+        {
+
+            context.MapRoute(
+                "auth_login",
+                "{culture}/dang-nhap",
+                new { culture = "vi", controller = "Account", action = "Login" },
+                new[] { "idn.AnPhu.Website.Areas.Auth.Controllers" }
+            );
+
+            context.MapRoute(
+                "auth_home",
+                "{culture}/quan-tri",
+                new { culture = "vi", controller = "Home", action = "Index" },
+                new[] { "idn.AnPhu.Website.Areas.Auth.Controllers" }
+            );
+
+            context.MapRoute(
+                "auth_sys_user",
+                "{culture}/quan-tri/nguoi-dung",
+                new { culture = "vi", controller = "Sys_User", action = "Index" },
+                new[] { "idn.AnPhu.Website.Areas.Auth.Controllers" }
+            );
+            context.MapRoute(
+                name: "auth_sys_user_create",
+                url: "{culture}/quan-tri/nguoi-dung/tao-moi",
+                defaults: new
+                {
+                    culture = "vi",
+                    area = "Auth",
+                    controller = "Sys_User",
+                    action = "Create",
+                },
+                namespaces: new[] { "idn.AnPhu.Website.Areas.Auth.Controllers" }
+            );
+
+            context.MapRoute(
+               name: "auth_sys_user_update",
+               url: "{culture}/quan-tri/nguoi-dung/cap-nhat",
+               defaults: new
+               {
+                   culture = "vi",
+                   area = "Auth",
+                   controller = "Sys_User",
+                   action = "Update",
+               },
+               namespaces: new[] { "idn.AnPhu.Website.Areas.Auth.Controllers" }
+           );
+            context.MapRoute(
+              name: "auth_sys_user_detail",
+              url: "{culture}/quan-tri/nguoi-dung/chi-tiet",
+              defaults: new
+              {
+                  culture = "vi",
+                  area = "Auth",
+                  controller = "Sys_User",
+                  action = "Detail",
+              },
+              namespaces: new[] { "idn.AnPhu.Website.Areas.Auth.Controllers" }
+          );
+
+
+
+
+
+
+
+            context.MapRoute(
+                "Auth_default",
+                "{culture}/Auth/{controller}/{action}/{id}",
+                new { culture = "vi", controller = "Home", action = "Index", id = UrlParameter.Optional },
+                new[] { "idn.AnPhu.Website.Areas.Auth.Controllers" }
+            );
+
+
+
+            //context.MapRoute(
+            //    "Auth_default",
+            //    "Auth/{controller}/{action}/{id}",
+            //    new { action = "Index", id = UrlParameter.Optional }
+            //);
+        }
+    }
+}
